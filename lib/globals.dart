df0305e031a86330f9c683b9555fd495ab1f829e
Varun Irani <42365289+VarunIrani@users.noreply.github.com>
2020-07-28 22:26:54 +0530

    Updated Express Server URL

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,6 +1,6 @@
 // * Put any keys or global variables here to access anywhere
 bool loggedIn = false;
-String serverURL = "https://2faffdff179c.ngrok.io";
+String serverURL = "http://deepfake.eastus.cloudapp.azure.com:3000";
 String name = "";
 String userId = "";
 String bearerToken = "";
