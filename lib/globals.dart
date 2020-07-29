8381f30da49d80243ec165d7ad3f1cdb6980fa85
Varun Irani <42365289+VarunIrani@users.noreply.github.com>
2020-07-29 19:08:25 +0530

    Update globals.dart

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,6 +1,6 @@
 // * Put any keys or global variables here to access anywhere
 bool loggedIn = false;
-String serverURL = "http://deepfake.eastus.cloudapp.azure.com:3000";
+String serverURL = "http://deepfake.westeurope.cloudapp.azure.com:3000";
 String name = "";
 String userId = "";
 String bearerToken = "";
